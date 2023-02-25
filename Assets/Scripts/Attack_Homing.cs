0ade6640634c2f90dce5f7f0ef010b67f0ac5bfd
Matimoo007 <mathieuvezina7@gmail.com>
2023-02-25 03:34:55 -0500

    homing start

--- Assets/Scripts/Attack_Homing.cs
+++ Assets/Scripts/Attack_Homing.cs
@@ -5,25 +5,35 @@ using UnityEngine;
 public class Attack_Homing : Boss_Attack
 {
     GameObject player;
+    Animator animator;
 
     private void Awake()
     {
         player = GameObject.Find("Player");
+        animator = GetComponent<Animator>();
     }
 
     protected override IEnumerator Attack()
     {
-        Vector2 target = new Vector2(-2.6f, 3.5f);
-        while (true)
+        Vector2 target = new Vector2(-2.6f, 2f);
+        while (Vector2.Distance(transform.position, target) > 0.01f)
         {
-
+            transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime);
         }
 
         while (true)
         {
-
+            transform.position = Vector2.Lerp(transform.position, player.transform.position, 2 * Time.deltaTime);
+            yield return null;
         }
+    }
 
-        AttackOver();
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" || collision.gameObject.layer == 6)
+        {
+            animator.Play("Hit", 0);
+            Invoke("AttackOver", .1f);
+        }
     }
 }
